bfcb109245b412abce425c6354b73cac130651d4
cdkrap123 <42858845+cdkrap123@users.noreply.github.com>
2023-04-06 15:59:51 -0500

    Add Files

---
+++ zip.php
@@ -0,0 +1,55 @@
+<?php
+$output = `rm -rf zip/*`;
+echo "<pre>$output</pre>";
+?>
+
+<?php
+$target_dir = "zip/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+//if ($_FILES["fileToUpload"]["size"] > 500000) {
+//  echo "Sorry, your file is too large.";
+//  $uploadOk = 0;
+//}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+?>
+
+<?php
+$test = NULL;
+$output = `file zip/map.json | grep 'zip/map.json: ASCII text, with very long lines, with no line terminators' > check.txt`;
+echo "<pre>$output</pre>";
+$test = shell_exec('cat check.txt');
+
+if (is_null($test))
+{
+    header("Location: /error.html");
+}
+else
+{
+    $output = `mv zip/map.json zip/map.wbox && pigz -z zip/map.wbox && mv zip/map.wbox.zz zip/map.wbox`;
+    echo "<pre>$output</pre>";
+    header("Location: /zip/map.wbox");
+    exit;
+}
+?>
